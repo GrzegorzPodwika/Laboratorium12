99be10bb25aca3319a383235c7af3a7a7824e8c9
Grzegorz Podwika <podwikagrzegorz@gmail.com>
2019-06-02 00:06:41 +0200

    Dodanie plikow projektu

---
+++ lab12/source.cpp
@@ -0,0 +1,17 @@
+#include "CSortTablicy.h"
+#include "CTablica.h"
+#include <iostream>
+using namespace std;
+
+//unsigned long long numberofComparisonns itd
+//
+int main()
+{
+	CSortTablicy tab;
+	tab.menu();
+
+	getchar();
+
+	return 0;
+}
+
